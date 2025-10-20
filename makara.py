e93b93ab30019d38da05d29ee2a6ce7ea22a53be
barisk802 <barisk802@gmail.com>
2025-10-21 01:46:18 +0300

    makara.py

--- makara.py
+++ makara.py
@@ -1,22 +1,19 @@
+import streamlit as st
 import webbrowser
 
-print("""
-İsmine Göre Hangi Şarkısın
-""")
+st.title("🎵 İsmine Göre Hangi Şarkısın")
 
-while True:
-    işlem=input("Lütfen İsminizi Giriniz :")
+isim = st.text_input("Lütfen İsminizi Giriniz:")
 
-    if işlem=="Cemre":
-        print("Şarkıya Yönlendiriliyorsunuz Lütfen Bekleyiniz...(SENİ ÇOOOOK SEVİYOORUMM)")
+if st.button("Şarkıyı Bul"):
+    if isim == "Cemre":
+        st.info("Şarkıya yönlendiriliyorsunuz... (SENİ ÇOOOOK SEVİYOORUMM 💖)")
         webbrowser.open("https://www.youtube.com/watch?v=rtOvBOTyX00&list=RDrtOvBOTyX00&start_radio=1")
-    elif işlem=="Mete":
-        print("Şarkıya Yönlendiriliyorsunuz Lütfen Bekleyiniz...")
+    elif isim == "Mete":
+        st.info("Şarkıya yönlendiriliyorsunuz...")
         webbrowser.open("https://www.youtube.com/watch?v=LU5FrAKJmYQ&list=RDLU5FrAKJmYQ&start_radio=1")
-    elif işlem=="Metehan":
-        print("Şarkıya Yönlen.. ahh Travmam tetiklendi ERROORR!! De..De..DEDEEM")
+    elif isim == "Metehan":
+        st.warning("Şarkıya yönlen... ahh Travmam tetiklendi ERROORR!! De..De..DEDEEM 😵‍💫")
         webbrowser.open("https://www.youtube.com/watch?v=sr6w-6tBTDk&list=RDsr6w-6tBTDk&start_radio=1")
-
     else:
-        print("İsminize göre şarkı bulunamadı :(")
-        break
+        st.error("İsminize göre şarkı bulunamadı 😔")
