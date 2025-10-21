28f273e82907d4e33cbb9beb57023cf0e47dc41e
barisk802 <barisk802@gmail.com>
2025-10-21 03:02:09 +0300

    Update makara.py

--- makara.py
+++ makara.py
@@ -1,25 +1,19 @@
-import streamlit as st
+isim_lower = isim.lower()
 
-st.title("🎵 İsmine Göre Hangi Şarkısın?")
-
-isim = st.text_input("Lütfen İsminizi Giriniz:")
-
-if st.button("Şarkıyı Bul"):
-    if isim == "Cemre" or "cemre":
-        st.success("SENİ ÇOOOOK SEVİYOORUMM ❤️")
-        st.markdown("[🎧 Şarkıya Git](https://www.youtube.com/watch?v=rtOvBOTyX00&list=RDrtOvBOTyX00&start_radio=1)")
-    elif isim == "Mete" or "mete":
-        st.info("🎶 Şarkıya yönlendiriliyorsunuz...")
-        st.markdown("[🎧 Şarkıya Git](https://www.youtube.com/watch?v=LU5FrAKJmYQ&list=RDLU5FrAKJmYQ&start_radio=1)")
-    elif isim == "Metehan" or "metehan":
-        st.warning("Şarkıya Yönlen.. ahh Travmam tetiklendi ERROORR!! De..De..DEDEEM 😅")
-        st.markdown("[😵‍💫 Şarkıya Git](https://www.youtube.com/watch?v=sr6w-6tBTDk&list=RDsr6w-6tBTDk&start_radio=1)")
-    elif isim == "Batu" or "Batuhan" or "batu" or "Batu" :
-        st.warning("Şarkıya gitmek için linke tıklayınız.")
-        st.markdown("[💛💙 Şarkıya Git](https://www.youtube.com/watch?v=NJ2TkBrWA04&list=RDNJ2TkBrWA04&start_radio=1)")
-    else:
-        st.error("İsminize göre şarkı bulunamadı :(")
-
+if isim_lower == "cemre":
+    st.success("SENİ ÇOOOOK SEVİYOORUMM ❤️")
+    st.markdown("[🎧 Şarkıya Git](https://www.youtube.com/watch?v=rtOvBOTyX00&list=RDrtOvBOTyX00&start_radio=1)")
+elif isim_lower == "mete":
+    st.info("🎶 Şarkıya yönlendiriliyorsunuz...")
+    st.markdown("[🎧 Şarkıya Git](https://www.youtube.com/watch?v=LU5FrAKJmYQ&list=RDLU5FrAKJmYQ&start_radio=1)")
+elif isim_lower == "metehan":
+    st.warning("Şarkıya Yönlen.. ahh Travmam tetiklendi ERROORR!! De..De..DEDEEM 😅")
+    st.markdown("[😵‍💫 Şarkıya Git](https://www.youtube.com/watch?v=sr6w-6tBTDk&list=RDsr6w-6tBTDk&start_radio=1)")
+elif isim_lower == "batu" or isim_lower == "batuhan":
+    st.warning("Şarkıya gitmek için linke tıklayınız.")
+    st.markdown("[💛💙 Şarkıya Git](https://www.youtube.com/watch?v=NJ2TkBrWA04&list=RDNJ2TkBrWA04&start_radio=1)")
+else:
+    st.error("İsminize göre şarkı bulunamadı :(")
 
 
 
